394650133816907119ffc009801f7daed1afa93b
Kianoush Azarnia <71564764+Kianoush-Azarnia@users.noreply.github.com>
2023-07-12 17:07:22 +0330

    first version
    Simple requirement, setting runtime , running status, image address ... with strings, not running containers actually yet

--- docker_platform/docker_app/views.py
+++ docker_platform/docker_app/views.py
@@ -1,7 +1,19 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import App, Run
 from .serializers import AppSerializer, RunSerializer
+
+
+class AppViewSet(viewsets.ModelViewSet):
+    queryset = App.objects.all()
+    serializer_class = AppSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
 
 class RunViewSet(viewsets.ModelViewSet):
     serializer_class = RunSerializer
@@ -14,10 +26,4 @@ class RunViewSet(viewsets.ModelViewSet):
         app_id = self.kwargs['pk']
         app = get_object_or_404(App, id=app_id)
         serializer.save(app=app)
-
-
-
-class AppViewSet(viewsets.ModelViewSet):
-    queryset = App.objects.all()
-    serializer_class = AppSerializer
 
--- docker_platform/docker_platform/urls.py
+++ docker_platform/docker_platform/urls.py
@@ -9,11 +9,11 @@ router.register(r'runs', RunViewSet, basename='run')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
-    
-    
-    path('api/', include([
-        re_path(r'^apps/(?P<pk>\d+)/runs/?$', RunViewSet.as_view({'post': 'create', 'get': 'list'}), name='run-app'),
-        re_path(r'^apps/(?P<pk>\d+)/runs/(?P<run_pk>\d+)/?$', RunViewSet.as_view({'get': 'retrieve'}), name='run-detail'),
-    ])),
+    re_path(r'^api/?$', include(router.urls)),
+    re_path(r'^api/apps/?$', AppViewSet.as_view({'get': 'list', 'post': 'create'}), name='app-list'),
+    re_path(r'^api/apps/(?P<pk>\d+)/?$', AppViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='app-detail'),
+    re_path(r'^api/apps/(?P<pk>\d+)/runs/?$', RunViewSet.as_view({'post': 'create', 'get': 'list'}), name='run-app'),
+    re_path(r'^api/apps/(?P<pk>\d+)/runs/(?P<run_pk>\d+)/?$', RunViewSet.as_view({'get': 'retrieve'}), name='run-detail'),
 ]
+
+urlpatterns += router.urls
