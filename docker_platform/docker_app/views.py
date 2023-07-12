5a09c38e40e5b2889deca68b1160f86d1d8f16a6
Kianoush Azarnia <71564764+Kianoush-Azarnia@users.noreply.github.com>
2023-07-12 15:04:42 +0330

    Update views.py

--- docker_platform/docker_app/views.py
+++ docker_platform/docker_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import App, Run
+from .serializers import AppSerializer, RunSerializer
+
+class AppViewSet(viewsets.ModelViewSet):
+    queryset = App.objects.all()
+    serializer_class = AppSerializer
+
+class RunViewSet(viewsets.ModelViewSet):
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
+
