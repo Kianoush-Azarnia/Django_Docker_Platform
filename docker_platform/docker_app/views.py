d6ed53e6518aaf7ce78b16d12c44c0b6b8f76fbd
Kianoush Azarnia <71564764+Kianoush-Azarnia@users.noreply.github.com>
2023-07-12 21:42:04 +0330

    Update views.py

--- docker_platform/docker_app/views.py
+++ docker_platform/docker_app/views.py
@@ -3,12 +3,22 @@ from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from .models import App, Run
 from .serializers import AppSerializer, RunSerializer
+from .tasks import download_docker_image, run_docker_command
 
 
 class AppViewSet(viewsets.ModelViewSet):
     queryset = App.objects.all()
     serializer_class = AppSerializer
 
+    def perform_create(self, serializer):
+        app = serializer.save()
+        # Trigger the task to download the Docker image
+        download_docker_image(app.id, app.image, f'static_root/{app.id}')
+        # Trigger the task to run the Docker command
+        run_docker_command(app.id)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
         self.perform_destroy(instance)
