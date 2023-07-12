52271692aec9a4d1fc8e12123d418f926d707600
Kianoush Azarnia <71564764+Kianoush-Azarnia@users.noreply.github.com>
2023-07-12 15:06:08 +0330

    Update urls.py

--- docker_platform/docker_platform/urls.py
+++ docker_platform/docker_platform/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from docker_app.views import AppViewSet, RunViewSet
+
+router = routers.DefaultRouter()
+router.register(r'apps', AppViewSet)
+router.register(r'runs', RunViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
 ]
