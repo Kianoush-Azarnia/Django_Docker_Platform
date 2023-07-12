21bd416a50d86266f87424403d866531a86a63f9
Kianoush Azarnia <71564764+Kianoush-Azarnia@users.noreply.github.com>
2023-07-12 14:57:44 +0330

    Update models.py

--- docker_platform/docker_app/models.py
+++ docker_platform/docker_app/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class App(models.Model):
     name = models.CharField(max_length=255)
-    image = models.CharField(max_length=255)
-    command = models.CharField(max_length=255)
+    image = models.CharField(max_length=1000)
+    command = models.CharField(max_length=1000)
 
 
 class EnvironmentVariable(models.Model):
@@ -16,5 +16,5 @@ class EnvironmentVariable(models.Model):
 class Run(models.Model):
     app = models.ForeignKey(App, related_name='runs', on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=255, choices=(('Running', 'Running'), ('Finished', 'Finished')))
-    parameters = models.CharField(max_length=255)
+    status = models.CharField(max_length=63, choices=(('Running', 'Running'), ('Finished', 'Finished')))
+    parameters = models.CharField(max_length=1000)
